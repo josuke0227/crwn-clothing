da208a00d56eae5b539a475e5dcd1a3365cd19da
Josuke <y.motosugi0227@gmail.com>
2021-03-21 22:05:56 +1100

    Add cartIcon and cart-dropdown components to our application, also imprement card redux slice

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CustomButton from "../custom-button/custom-button.component";
+
+import "./cart-dropdown.styles.scss";
+
+const CardDropDown = () => (
+  <div className="cart-dropdown">
+    <div className="cart-items" />
+    <CustomButton>GO TO CHECK OUT</CustomButton>
+  </div>
+);
+export default CardDropDown;
